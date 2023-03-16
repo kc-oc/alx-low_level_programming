1e6da4667c8f55c61f661c52c8b04953c8366efe
ocansey <karenocansey67@gmail.com>
2023-03-16 01:29:19 -0700

    lopp

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -4,7 +4,6 @@
  * Return: 0 (Success)
  */
 int main(void)
-{
-        printf("with proper grammer, but the outcome is a piece of art,\n");
+{ printf("with proper grammer, but the outcome is a piece of art,\n");
 	return (0);
 }
