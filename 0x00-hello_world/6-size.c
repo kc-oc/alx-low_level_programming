a5cb7335df71861958642be8f4de38fac5c14c97
ocansey <karenocansey67@gmail.com>
2023-03-16 02:59:58 -0700

    pools

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -5,12 +5,17 @@
  * Return: Always 0 (Success)
  */
 int main(void)
-
 {
-	printf("Size of a char: %1d byte(s)\n", sizeof(char));
-	printf("Size of an int: %1d byte(s)\n", sizeof(int));
-	printf("Size of a long int: %1d byte(s)\n", sizeof(long int));
-	printf("Size of a long long int: %1d byte(s)\n", sizeof(long long int));
-	printf("Size of a float: %1d  byte(s)\n", sizeof(float));
-	return (0);
+	char a:
+	int b;
+        long int c;
+        long long int d;
+        float f;
+
+printf("Size of a char: %1d byte(s)\n", (unsigned long)sizeof(a));
+printf("Size of an int: %1d byte(s)\n", (unsigned long)sizeof(b));
+printf("Size of a long int: %1d byte(s)\n", (unsigned long)sizeof(c));
+printf("Size of a long long int: %1d byte(s)\n", (unsigned long)sizeof(d));
+printf("Size of a float: %1d  byte(s)\n", (unsigned long)sizeof(f));
+return (0);
 }
