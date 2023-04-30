2cb144bc86a242c44459485cc2b1cf47b3a1b78c
ocansey <karenocansey67@gmail.com>
2023-04-30 05:30:55 -0700

    hopes and dreams1

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -4,7 +4,7 @@
  * pop_listint - This is used to delete the head node of a linked list.
  * @head: This is the pointer to the first element in the linked list.
  *
- * Return: The data inside this_elements that was deleted or 0 if the -list is empty.
+ * Return: The data inside this_elements that was deleted or 0 if the list is empty.
  */
 
 int pop_listint(listint_t **head)
