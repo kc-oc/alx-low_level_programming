c5a9d00feb7ccd04f04b5a4999c82cb62123a918
ocansey <karenocansey67@gmail.com>
2023-05-10 00:54:15 -0700

    lol

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -8,22 +8,20 @@
  * 
  * @letters: This is the number of letters to be read.
  *
- * Return: This is the w- actual number of bytes read and printed.
- *        0 when function fails or filename is NULL.
+ * Return: This is the w- actual number of bytes read and printed 0 when function fails or filename is NULL.
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 	char *buf;
+	ssize_t fd;
 	ssize_t t;
 	ssize_t k;
-	ssize_t fd;
 
 
 	fd = open(filename, O_RDONLY);
 	if (fd == -1)
 	return (0);
-	
-	
+		
 	
 	buf = malloc(sizeof(char) * letters);
 	t = read(fd, buf, letters);
