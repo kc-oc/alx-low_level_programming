68d33de158f49e9cd7f0fa1ff76b8f9359e934b8
ocansey <karenocansey67@gmail.com>
2023-05-23 04:12:27 -0700

    yui

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,40 +1,38 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <fcntl.h>
-#include <unistd.h>
+#include "main.h"
 
 /**
- * append_text_to_file - This appends text at the end of a file
- * @filename: the name of the file
- * @text_content: the NULL terminated string to add at the end of the file
+ * create_file - This creates a file
+ * @filename: The filename.
+ * @text_content: This is the content writed in the file.
  *
- * Return: 1 on success, -1 on failure
+ * Return: 1 if it success. -1 if it fails.
  */
-int append_text_to_file(const char *filename, const char *text_content)
+int create_file(const char *filename, char *text_content)
 {
-	int fdr;
-	ssize_t nletters;
-	ssize_t o;
+	int ff;
+	int nletters;
+	int rr;
 
 	if (!filename)
 		return (-1);
 
-	fdr = open(filename, O_WRONLY | O_APPEND);
-	if (fdr == -1)
+	ff = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+
+	if (ff == -1)
 		return (-1);
 
-	if (text_content)
-	{
-		nletters = strlen(text_content);
-		o = write(fdr, text_content, nletters);
-		if (o == -1)
-		{
-			close(fdr);
-			return (-1);
-		}
-	}
+	if (!text_content)
+		text_content = "";
 
-	close(fdr);
+	for (nletters = 0; text_content[nletters]; nletters++)
+		;
+
+	rr = write(fd, text_content, nletters);
+
+	if (rr == -1)
+		return (-1);
+
+	close(ff);
+
 	return (1);
 }
